7bfbb18c5507437afb99cb9cca52fae149b68930
Survivian <32820657+Survivian@users.noreply.github.com>
2021-08-03 17:11:50 -0400

    The switch to ip

--- clip2.py
+++ clip2.py
@@ -2,12 +2,13 @@
 import os
  
 # The path for listing items
-path = r"D:/Everything Else/The Images/Pennys Problem"
-url = r"http://192.168.1.92/The%20Images/Pennys%20Problem/" 
+path = r"D:\Everything Else\The Images\Pennys Problem"
+url = r"http://66.168.178.255/The%20Images/Pennys%20Problem/" 
 # The list of items
 files = os.listdir(path)
  
 # Loop to print each filename separately
+files = sorted(files)
 for filename in files:
     filename = filename.replace(" ", "%20")
     fullname = url + filename
