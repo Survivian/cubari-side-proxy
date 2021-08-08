418a28aac1a838fe4c4a1f81656156245494db38
Survivian <32820657+Survivian@users.noreply.github.com>
2021-08-08 01:24:33 -0400

    Kincaid Remove

--- dl.py
+++ dl.py
@@ -1,4 +1,4 @@
 # Needed imports
 import subprocess
-url = "https://kemono.party/patreon/user/10734570/post/51222189"
-subprocess.run(["gallery-dl", '-d', 'D:\Everything Else', url, '--zip'], shell=True)
+url = "https://kemono.party/patreon/user/10734570/post/39763448"
+subprocess.run(["gallery-dl", '-d', 'D:\Everything Else', url], shell=True)
